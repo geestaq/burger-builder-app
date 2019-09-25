5da9987a182188721e8cdb6b380af8c316625037
geestaq <gee@vp.pl>
2019-09-25 14:28:19 +0200

    Add burger price

--- src/components/features/BuildControls/BuildControls.js
+++ src/components/features/BuildControls/BuildControls.js
@@ -11,6 +11,7 @@ const controls = [
 
 const buildControls = (props) => (
   <div className='build-controls'>
+    <p>Current price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map(item => (
       <BuildControl
         key={item.label}
