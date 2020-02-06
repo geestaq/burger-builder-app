7cb7a49289d16983050b1c06d2015bb55b9424ec
geestaq <gee@vp.pl>
2020-02-06 20:51:10 +0100

    Add axios and post method to save order to backend

--- src/components/features/BurgerBuilder/BurgerBuilder.js
+++ src/components/features/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Modal from '../../common/Modal/Modal';
 import Burger from '../../features/Burger/Burger';
 import BuildControls from '../../features/BuildControls/BuildControls';
 import OrderSummary from '../../features/OrderSummary/OrderSummary';
+import axios from '../../../axios-orders.js';
 
 const INGREDIENT_PRICES = {
   salad: 2,
@@ -74,7 +75,23 @@ class BurgerBuilder extends React.Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('You continue!');
+    //alert('You continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'geestaq',
+        address: {
+          street: 'Testowa',
+          zipCode: 1234,
+          city: 'Testowo'
+        },
+        email: 'test@test.com'
+      }
+    };
+    axios.post('/orders.json', order)
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
   }
 
   render() {
