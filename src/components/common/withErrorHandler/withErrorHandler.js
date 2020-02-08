7e576f6c1d93c5206b8697ca8391fa112e16228a
geestaq <gee@vp.pl>
2020-02-08 12:02:13 +0100

    Add interceptors removal on component unmount

--- src/components/common/withErrorHandler/withErrorHandler.js
+++ src/components/common/withErrorHandler/withErrorHandler.js
@@ -10,13 +10,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     UNSAFE_componentWillMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(res => res, error => {
+      this.respInterceptor = axios.interceptors.response.use(res => res, error => {
         this.setState({ error: error });
       })
+    }
+
+    componentWillUnmount() {
+      //usuniecie interceptorow przy usuwaniu komponentu - nie zajmuja pamieci
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.respInterceptor);
     }
 
     errorConfirmedHandler = () => {
